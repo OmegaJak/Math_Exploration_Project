ccff5bc511f914f12fb676675a4b677ec2906305
OmegaJak <jak.kruger@gmail.com>
2014-06-06 11:34:51 -0500

    Added options in main loop for what to do

--- Math Exploration Project/src/ExplorationProject.java
+++ Math Exploration Project/src/ExplorationProject.java
@@ -10,13 +10,33 @@ public class ExplorationProject {
 		Differentiator differentiator = new Differentiator();
 		boolean shouldGoAgain = true;
 		while (shouldGoAgain) {
-			System.out.println("Please enter the equation with the proper format:");
-//			System.out.println("Please enter two terms to be simplified");
-			differentiator.analyze(getReadLine());
-//			differentiator.simplifyTerms(getReadLine(), getReadLine(), 3);
+			System.out.println("What would you like to do?");
+			System.out.println("\'d\' for differentiation, \'m\' for multiplication"
+								+ "\'div\' for division, \'add\' for division, \'sub\' for subtraction");
+			switch(getReadLine()) {
+				case "d":
+					System.out.println("Please enter the equation with the proper format:");
+					differentiator.analyze(getReadLine());
+					break;
+				case "m":
+					System.out.println("Please enter two terms to be multiplied");
+					System.out.println(differentiator.simplifyTerms(getReadLine(), getReadLine(), 0));
+					break;
+				case "div":
+					System.out.println("Please enter two terms to be divided");
+					System.out.println(differentiator.simplifyTerms(getReadLine(), getReadLine(), 1));
+					break;
+				case "add":
+					System.out.println("Please enter two terms to be added");
+					System.out.println(differentiator.simplifyTerms(getReadLine(), getReadLine(), 2));
+					break;
+				case "sub":
+					System.out.println("Please enter two terms to be subtracted");
+					System.out.println(differentiator.simplifyTerms(getReadLine(), getReadLine(), 3));
+					break;
+			}
 //			differentiator.canAddOrSubtract(getReadLine(), getReadLine());
 			differentiator.clearArrays();
-			System.out.println("Success!");
 			System.out.println("Again?");
 			String response = getReadLine();
 			if (!response.equals("yes")) {
